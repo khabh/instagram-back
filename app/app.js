605cc9f6e06c4c1bc434831c4b1d647f5d470d51
JUHA <khabh@naver.com>
2022-07-12 22:32:39 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -8,9 +8,10 @@ const app = express();
 const post = require("./src/apis/post");
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/views`));
 app.use("/", post); //미들웨어 등록
 
 module.exports = app;
